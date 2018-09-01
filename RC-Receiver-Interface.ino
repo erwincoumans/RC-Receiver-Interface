3ade50f12b2efa4b5a3d43a11069595bc8370f7a
erwincoumans <erwin.coumans@gmail.com>
2018-08-31 19:04:18 -0700

    Script to make Quanum RC control as joystick
    Mainly for MIT Racecar / F1-10, see http://f1tenth.org/
    Will replace Logitech F710 with buttons 4 and 5 and steering/throttle for 'racecar' experiments.

--- RC-Receiver-Interface.ino
+++ RC-Receiver-Interface.ino
@@ -1,39 +1,36 @@
 #include "./include/Receiver.h"
-
-#include "./include/PPM_Receiver.h"
 #include "./include/PWM_Receiver.h"
 
 PWMReceiver pwmReceiver(19, 20, 21, 22);
-PPMReceiver ppmReceiver(10);
-
-#define use_pwm
 
 uint16_t channels[4] = { 0 };
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
 }
 
 void loop() {
-#ifdef use_pwm
     pwmReceiver.update(channels);
     for (int index = 0; index < 4; index++) {
-        Serial.print(channels[index]);
-        Serial.print("\t");
+        //Serial.print(channels[index]);
+        //Serial.print("\t");
     }
-    Serial.println();
-    delay(5);
-#endif
 
-#ifdef use_ppm
+  Joystick.X(512);//channels[0]);//analogRead(0));
+  Joystick.Y(channels[1]);//forward accelerate/velocity
+  Joystick.Z(512);
+  Joystick.Zrotate(channels[0]);//steering
 
-    ppmReceiver.update(channels);
-    for (int index = 0; index < 8; index++) {
-        Serial.print(channels[index]);
-        Serial.print("\t");
-    }
-    Serial.println();
-    delay(5);
+  Joystick.slider(512);//channels[1]);
 
-#endif
+  Joystick.sliderLeft(512);//channels[1]);
+  Joystick.sliderRight(512);//channels[1]);
+
+//button '4' (called 5) on
+  Joystick.button(5, channels[2]<200? 1 : 0);
+
+//button '5' (called 6) on
+  Joystick.button(6, channels[2]>500? 1 : 0);
+  
+    delay(50);
 }
